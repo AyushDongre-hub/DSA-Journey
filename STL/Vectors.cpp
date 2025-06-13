f9617d642aa7bc0d0773a118156ba09217f2b887
Ayush <ayush192005@kgpian.iitkgp.ac.in>
2025-06-14 01:15:55 +0530

    Vectors STL

--- STL/Vectors.cpp
+++ STL/Vectors.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
 int main() {
     vector<int> vec;
+    vec.push_back(1);
+    vec.push_back(2);      //Add values
+    vec.push_back(3);
+
+    for(int var:vec){
+        cout << var << "";//Print Values
+    }
+    
+    vec.pop_back();       //Remove from last
+
+    cout << vec[1] << " or " << vec.at(1);   //Same
 
 
     return 0;
